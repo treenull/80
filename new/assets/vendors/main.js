8608bc3d0e742c594444791133f6db80b422f325
onlywyj <wyj669666@outlook.com>
2021-08-29 22:45:19 +0800

    前端获取ip地址等，数据回传记录

--- new/assets/vendors/main.js
+++ new/assets/vendors/main.js
@@ -1,6 +1,5 @@
 
 $(document).ready(function () {
-
 	// 获取一言数据
 	fetch('https://v1.hitokoto.cn').then(function (res) {
 		return res.json();
@@ -10,5 +9,23 @@ $(document).ready(function () {
 	}).catch(function (err) {
 		console.error(err);
 	})
+
 });
-	
+
+
+
+function post(){
+    $.ajax({
+        url: "../../apppubServer/checkIp",
+        type: "POST",
+        data: {
+            id:$('#edit_ser_id').val(),
+            ip: $('#edit_ser_ip').val()
+        },
+        success: function (data){
+            if(!data.success){
+                $('#judge_ip').text('服务器地址重复');
+            }
+        }
+    });
+}
