9a142f6c24c929a80fb2c525f23c5e69a452f576
onlywyj <wyj669666@outlook.com>
2021-08-31 00:16:59 +0800

    浏览器同源问题待解决，基本功能完善

--- new/assets/vendors/main.js
+++ new/assets/vendors/main.js
@@ -1,44 +1,26 @@
 
 $(document).ready(function () {
-    var hitokoto = null;
-    // 获取一言数据
     fetch('https://v1.hitokoto.cn').then(function (res) {
         return res.json();
     }).then(function (e) {
-        hitokoto = e
         $('#description').html(e.hitokoto)
         $('#dfrom').html("——「" + e.from + "」")
+        dataRecord(e);
     }).catch(function (err) {
         console.error(err);
     });
-
-    fetch('http://127.0.0.1:9282/cloudopen/yiyan/checkHealth', {
-        method: 'GET',
-        mode: 'cors',
-    }).then(function (res) {
-        return res.json();
-    }).then(function (e) {
-        console.log(e);
-        // $('#description').html(e.hitokoto)
-        // $('#dfrom').html("——「" + e.from + "」")
-    }).catch(function (err) {
-        console.error(err);
-    });
-
-    const url = 'http://127.0.0.1:9282/cloudopen/yiyan/dataRecord';
-    var data = {hitokoto};
-
-    fetch(url, {
-        method: 'POST', // or 'PUT'
-        mode: 'cors',
-        body: JSON.stringify(data), // data can be `string` or {object}!
-        headers: new Headers({
-          'Content-Type': 'application/json'
-        })
-      }).then(res => res.json())
-      .catch(error => console.error('Error:', error))
-      .then(response => console.log('Success:', response));
-
 });
 
-
+function dataRecord(data){
+    const url = 'http://121.41.90.69:9282/cloudopen/yiyan/dataRecord';
+        fetch(url, {
+            method: 'POST',
+            mode: 'cors',
+            body: JSON.stringify(data),
+            headers: new Headers({
+              'Content-Type': 'application/json'
+            })
+          }).then(res => res.json())
+          .catch(error => console.error('Error:', error))
+          .then(response => console.log('Success:', response));
+}
