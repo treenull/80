5ca9e3a141d05469a0e0dea5872f9a55cd6e0fad
treenull <wyj669666@outlook.com>
2024-05-13 09:56:03 +0800

    数据库替换为MySQL，提升安全性校验，删除swagger功能，精简服务

--- open/cloudopen/src/main/java/com/wyj/cloudopen/config/SwaggerConfig.java
+++ open/cloudopen/src/main/java/com/wyj/cloudopen/config/SwaggerConfig.java
@@ -1,56 +1,56 @@
-package com.wyj.cloudopen.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-/**
- * @Author WYJ
- * @Data 2021/9/16 10:42
- * @Description:
- *      Swagger2配置类
- *      在与spring boot集成时，放在与Application.java同级的目录下。
- *      或者通过 @Import 导入配置
- */
-@Configuration
-@EnableSwagger2
-public class SwaggerConfig {
-
-    /**
-     * 创建API应用
-     * apiInfo() 增加API相关信息
-     * 通过select()函数返回一个ApiSelectorBuilder实例,用来控制哪些接口暴露给Swagger来展现，
-     * 本例采用指定扫描的包路径来定义指定要建立API的目录。
-     * @return
-     */
-    @Bean
-    public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.wyj.cloudopen.controller"))
-                .paths(PathSelectors.any())
-                .build();
-    }
-
-    /**
-     * 创建该API的基本信息（这些基本信息会展现在文档页面中）
-     * 访问地址：http://项目实际地址/swagger-ui.html
-     * @return
-     */
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Spring Boot中使用Swagger2构建RESTful APIs")
-                .description("快捷调试接口")
-                .termsOfServiceUrl("")
-                .version("1.0")
-                .build();
-    }
-
-}
+//package com.wyj.cloudopen.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import springfox.documentation.builders.ApiInfoBuilder;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.service.ApiInfo;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//
+///**
+// * @Author WYJ
+// * @Data 2021/9/16 10:42
+// * @Description:
+// *      Swagger2配置类 默认访问地址 https://open.treenull.cn/swagger-ui.html#/
+// *      在与spring boot集成时，放在与Application.java同级的目录下。
+// *      或者通过 @Import 导入配置
+// */
+//@Configuration
+//@EnableSwagger2
+//public class SwaggerConfig {
+//
+//    /**
+//     * 创建API应用
+//     * apiInfo() 增加API相关信息
+//     * 通过select()函数返回一个ApiSelectorBuilder实例,用来控制哪些接口暴露给Swagger来展现，
+//     * 本例采用指定扫描的包路径来定义指定要建立API的目录。
+//     * @return
+//     */
+//    @Bean
+//    public Docket createRestApi() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .apiInfo(apiInfo())
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.wyj.cloudopen.controller"))
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
+//
+//    /**
+//     * 创建该API的基本信息（这些基本信息会展现在文档页面中）
+//     * 访问地址：http://项目实际地址/swagger-ui.html
+//     * @return
+//     */
+//    private ApiInfo apiInfo() {
+//        return new ApiInfoBuilder()
+//                .title("Spring Boot中使用Swagger2构建RESTful APIs")
+//                .description("快捷调试接口")
+//                .termsOfServiceUrl("")
+//                .version("1.0")
+//                .build();
+//    }
+//
+//}
